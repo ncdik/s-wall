03abf30f025442f0524b1bc5b49e27b07ff7c97a
koseg <kosegorkan2@yandex.ru>
2018-09-21 11:34:35 +0700

    update model Message

--- app/Message.php
+++ app/Message.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
 {
-    //
+    protected $fillable = ['user_id', 'text', 'crypted'];
+    
+    public function user(){
+        return $this->hasOne('App\User', 'id', 'user_id');
+    }
 }
